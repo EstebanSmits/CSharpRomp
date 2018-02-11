253727811b63a2fdfa854f3e1cc8eb6e95067256
daxmax <daxtrash@yahoo.com>
2018-02-11 03:36:30 -0600

    added DI to project

--- CSharpRomp/Program.cs
+++ CSharpRomp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using CSharpRomp.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CSharpRomp
 {
@@ -6,7 +10,32 @@ namespace CSharpRomp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         
+            IServiceCollection services = new ServiceCollection();
+            // Startup.cs finally :)
+            var startup = new Startup();
+            startup.ConfigureServices(services);
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            //configure console logging
+            serviceProvider
+                .GetService<ILoggerFactory>()  ;
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+
+            logger.LogDebug("Logger is working!");
+
+            // Get Service and call method
+            var service = serviceProvider.GetService<ILocallizedServices>();
+             service.MyServiceMethod();
+            var configuration= serviceProvider.GetService<IConfigurationRoot>();
+
+
+            Console.WriteLine(configuration.GetConnectionString("daxmaxdb"));
+            var _cities = new Cities();
+
+            Console.ReadLine();
         }
     }
 }
