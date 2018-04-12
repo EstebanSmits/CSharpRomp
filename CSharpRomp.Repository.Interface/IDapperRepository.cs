760d4e8fe197722b71991b1769751522b690d7f7
daxmax@gmail.com <daxmax@gmail.com>
2018-04-12 02:18:10 -0500

    updated files
    Signed-off-by: daxmax@gmail.com <daxmax@gmail.com>

---
+++ CSharpRomp.Repository.Interface/IDapperRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharpRomp.Repository.Interface
+{
+    public interface IDapperRepository
+    {
+        Task<T> GetRecord<T>(string sql, object parameters = null);
+        Task<IEnumerable<T>> GetRecords<T>(string sql, object parameters = null);
+    }
+}
